d8236a978e63d1eecff765aaec781ef6b20b518e
Ido Aharon <ido991155@gmail.com>
2025-05-11 21:26:47 +0300

    start adding business

--- src/pages/BusinessPage.jsx
+++ src/pages/BusinessPage.jsx
@@ -1,10 +1,19 @@
-import { Box, Grid2 as Grid } from "@mui/material";
+import { Box, Grid2 as Grid, Stack, TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
-
+// TODO - add onchange working hours.
+// TODO - do add business
 const BusinessPage = (props) => {
 
     const [businesses, setBusinesses] = useState([]);
+    const [newBusiness, setNewBusiness] = useState({
+        name:"",
+        phoneNumber:"",
+        address:"",
+        workingHours:{from:0, to:0}
+        
+
+    });
 
     useEffect(() => {
         //מקבלים את המידע מהשרת
@@ -20,8 +29,19 @@ const BusinessPage = (props) => {
 
     }, []);
 
+
+    const changeItem = (ev) => {
+
+        let {value, name} = ev.target;
+        setNewBusiness((prevState => {
+            return {...prevState, [name]: value};
+        }));
+    }
+
     return (
         <>
+
+       
             <Box component="div" className="page1" >
                 <Box component="div" className="area">name: shuki barbar style</Box>
                 <Box component="div" className="area">Business phone number</Box>
@@ -71,8 +91,8 @@ const BusinessPage = (props) => {
             {/* כאן רואים את האייטמים */}
             {businesses.map(item => {
                 return (
-                    <Box component="div" className="page1" key = {item.id}>
-                        <Box component="div" className="area">name: {item.name}</Box>
+                    <Box component="div" className="page1" key={item.id}>
+                        <Box component="div" className="area" >name: {item.name}</Box>
                         <Box component="div" className="area">Business phone number: {item.phoneNumer}</Box>
                         <Box component="div" className="area">Business address: {item.address}</Box>
                         <Box component="div" className="area">Your working hours</Box>
@@ -80,9 +100,23 @@ const BusinessPage = (props) => {
                     </Box>
                 );
             })}
+            <Stack direction="column"
+                spacing={2}
+                sx={{
+                    justifyContent: "center",
+                    alignItems: "flex-start",
+                }} >
+                <TextField id="outlined-basic" label="name:" variant="outlined" name="name" onChange={changeItem}/>
+                <TextField id="outlined-basic" label="Business phone number: " variant="outlined"  name="phoneNumber" onChange={changeItem}/>
+                <TextField id="outlined-basic" label="Business address:" variant="outlined" name="address"onChange={changeItem} />
+                <h2>Working hours</h2>
+                <TextField id="outlined-basic" label="from:" variant="outlined" type="number" name="from" />
+                <TextField id="outlined-basic" label="to:" variant="outlined" type="number" name="to"/>
+                <p>
+            state new business = {newBusiness.name}, address = {newBusiness.address}, phone = {newBusiness.phoneNumber}
 
-
-
+        </p>
+            </Stack>
         </>
     )
 
