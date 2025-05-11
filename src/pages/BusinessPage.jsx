d2f4829f6eac61e6cb47454210e979f31f0c5742
Ido Aharon <ido991155@gmail.com>
2025-05-11 20:16:41 +0300

    -showing business

--- src/pages/BusinessPage.jsx
+++ src/pages/BusinessPage.jsx
@@ -1,26 +1,28 @@
 import { Box, Grid2 as Grid } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 
 const BusinessPage = (props) => {
 
-    useEffect(()=>{
+    const [businesses, setBusinesses] = useState([]);
 
+    useEffect(() => {
+        //מקבלים את המידע מהשרת
         const getData = async () => {
-        const tempResponse = await fetch("/api/business");
-        let tempData = await tempResponse.json();
-        console.log("my data business = ", tempData);
-        
+            const tempResponse = await fetch("/api/business");
+            let tempData = await tempResponse.json();
+            console.log("my data business = ", tempData);
+            setBusinesses(tempData);
 
         }
         getData();
 
 
-    },[]);
+    }, []);
 
     return (
         <>
-            <Box component="div" className="page1">
+            <Box component="div" className="page1" >
                 <Box component="div" className="area">name: shuki barbar style</Box>
                 <Box component="div" className="area">Business phone number</Box>
                 <Box component="div" className="area">Business address</Box>
@@ -63,6 +65,23 @@ const BusinessPage = (props) => {
                 <Box component="div" className="area">Workers</Box>
                 <Box component="div" className="area">Let's design your business page</Box>
             </Box>
+
+            <h1>Here the real area: </h1>
+
+            {/* כאן רואים את האייטמים */}
+            {businesses.map(item => {
+                return (
+                    <Box component="div" className="page1" key = {item.id}>
+                        <Box component="div" className="area">name: {item.name}</Box>
+                        <Box component="div" className="area">Business phone number: {item.phoneNumer}</Box>
+                        <Box component="div" className="area">Business address: {item.address}</Box>
+                        <Box component="div" className="area">Your working hours</Box>
+                        <Box component="div" className="area">Adding services:</Box>
+                    </Box>
+                );
+            })}
+
+
 
         </>
     )
